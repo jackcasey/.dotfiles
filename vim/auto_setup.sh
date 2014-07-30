3a4d29e7e0fda9e8a48bd99809155437f8aa719d
Jack Casey <jack.casey@agworld.com.au>
2014-07-30 15:51:05 +0800

    Updated vim. Moved to auto symlinks.

--- vim/auto_setup.sh
+++ vim/auto_setup.sh
@@ -1,1 +1,4 @@
-ln -i -s $HOME/.dotfiles/vim/vimrc.symlink $HOME/.vimrc
+# NeoBundle
+if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ]; then
+  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh 
+fi
