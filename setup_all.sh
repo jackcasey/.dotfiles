7f393a0fb5338f04fd070b837353f10830cd7c6e
Jack Casey <jack.casey@agworld.com.au>
2014-07-30 16:32:34 +0800

    Just tidied up the setup_all script slightly more.

--- setup_all.sh
+++ setup_all.sh
@@ -1,14 +1,15 @@
 pushd $HOME/.dotfiles
 
 echo 'Linking all symlinks into homedir:'
-echo "  NB: I'll skip over any that are already symlinks"
 for i in `find \`pwd\` -iname '*.symlink'`; do
   file=$(basename $i)
   file=.${file%%.symlink}
   homefile=$HOME/$file
   if [ ! -L $homefile ]; then
-    echo "$i -> $homefile"
+    echo "  $i -> $homefile"
     ln -s -i $i $homefile
+  else
+    echo "  $homefile is already a symlink"
   fi
 done
 
