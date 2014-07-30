9ebd78481f394bace0c290569a6fdcd2d011f48d
Jack Casey <jack.casey@agworld.com.au>
2014-07-30 15:29:59 +0800

    setup_all.sh now symlinks dotfiles to homedir automatically

--- setup_all.sh
+++ setup_all.sh
@@ -1,7 +1,14 @@
 pushd $HOME/.dotfiles
 
-echo 'Calling all auto setups:'
+echo 'Linking all symlinks into homedir:'
+for i in `find \`pwd\` -iname '*.symlink'`; do
+  file=$(basename $i)
+  file=.${file%%.symlink}
+  echo "$i -> $HOME/$file"
+  ln -s -i $i $HOME/$file
+done
 
+echo 'Calling all auto setups:'
 for i in `find . -iname 'auto_setup.sh' -maxdepth 2`; do
   echo "  ${i:2}"
   bash $i
