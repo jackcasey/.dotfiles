acccd032077c69166fd71645119b9bdbbbd65339
Jack Casey <jack.casey@agworld.com.au>
2014-11-20 16:12:51 +0800

    Added ctags support and some aliases

--- vim/auto_setup.sh
+++ vim/auto_setup.sh
@@ -1,4 +1,5 @@
 command -v ag >/dev/null 2>&1 || { echo >&2 "    Please install ag to enable unite searching."; exit 1; }
+command -v ctags >/dev/null 2>&1 || { echo >&2 "    Please install ctags to enable vim ctags."; exit 1; }
 
 # NeoBundle
 if [ ! -d "$HOME/.vim/bundle/neobundle.vim" ]; then
