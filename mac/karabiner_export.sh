09401a64352ab884494a2ff4399c51506fa97f99
Jack Casey <jack.casey@agworld.com.au>
2015-01-21 15:20:51 +0800

    Karabiner - Removed a bunch of extra things I don't use or need

--- mac/karabiner_export.sh
+++ mac/karabiner_export.sh
@@ -2,19 +2,9 @@
 
 cli=/usr/local/bin/karabiner
 
-$cli set repeat.initial_wait 200
-/bin/echo -n .
-$cli set private.control_space_is_underscore 1
-/bin/echo -n .
-$cli set remap.consumer_to_fkeys_f5 1
-/bin/echo -n .
-$cli set remap.hjkl_arrow 1
-/bin/echo -n .
-$cli set option.tabmode_hl 1
-/bin/echo -n .
-$cli set remap.tabmode 1
-/bin/echo -n .
 $cli set repeat.wait 18
+/bin/echo -n .
+$cli set repeat.initial_wait 300
 /bin/echo -n .
 $cli set remap.simple_vi_mode 1
 /bin/echo -n .
@@ -22,6 +12,8 @@ $cli set parameter.holdingkeytokey_wait 150
 /bin/echo -n .
 $cli set private.ctrl_space_for_underscore 1
 /bin/echo -n .
-$cli set option.tabmode_uo 1
+$cli set private.control_space_is_underscore 1
+/bin/echo -n .
+$cli set remap.consumer_to_fkeys_f5 1
 /bin/echo -n .
 /bin/echo
