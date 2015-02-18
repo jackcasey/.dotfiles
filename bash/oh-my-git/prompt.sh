a11f80fdfeca17e6b26dd0c58fd575d91f8a2b2f
Jack Casey <jack.casey@agworld.com.au>
2015-02-18 21:36:21 +0800

    Git - Changed symbols and colours, added help to oh-my-git prompt

--- bash/oh-my-git/prompt.sh
+++ bash/oh-my-git/prompt.sh
@@ -2,43 +2,15 @@ PSORG=$PS1;
 if [ -n "${BASH_VERSION}" ]; then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-    # Symbols
-    : ${is_a_git_repo_symbol:='❤'}
-    : ${has_untracked_files_symbol:='∿'}
-    : ${has_adds_symbol:='+'}
-    : ${has_deletions_symbol:='-'}
-    : ${has_deletions_cached_symbol:='✖'}
-    : ${has_modifications_symbol:='✎'}
-    : ${has_modifications_cached_symbol:='☲'}
-    : ${ready_to_commit_symbol:='→'}
-    : ${is_on_a_tag_symbol:='⌫'}
-    : ${needs_to_merge_symbol:='ᄉ'}
-    : ${has_upstream_symbol:='⇅'}
-    : ${detached_symbol:='⚯ '}
-    : ${can_fast_forward_symbol:='»'}
-    : ${has_diverged_symbol:='Ⴤ'}
-    : ${rebase_tracking_branch_symbol:='↶'}
-    : ${merge_tracking_branch_symbol:='ᄉ'}
-    : ${should_push_symbol:='↑'}
-    : ${has_stashes_symbol:='★'}
+    source ${DIR}/symbols.sh
 
     # Flags
-    : ${display_has_upstream:=false}
-    : ${display_tag:=false}
-    : ${display_tag_name:=true}
-    : ${two_lines:=true}
-    : ${finally:=$PROMPT_BASE}
-    : ${use_color_off:=false}
-
-    # Colors
-    : ${on='\[\033[1;37m\]'}
-    : ${off='\[\033[0m\]'}
-    : ${red='\[\033[0;31m\]'}
-    : ${green='\[\033[0;32m\]'}
-    : ${yellow='\[\033[1;33m\]'}
-    : ${violet='\[\033[0;35m\]'}
-    : ${branch_color='\[\033[1;34m\]'}
-    : ${reset='\[\033[0m\]'}
+    : ${display_no_upstream=true}
+    : ${display_tag=true}
+    : ${display_tag_name=true}
+    : ${two_lines=true}
+    : ${finally=$PROMPT_BASE}
+    : ${use_color_off=false}
 
     PS2="${yellow}→${reset} "
 
---
+++ bash/oh-my-git/symbols.sh
@@ -0,0 +1,30 @@
+# Symbols
+: ${is_a_git_repo_symbol='♪'}
+: ${has_stashes_symbol='◈'}
+: ${has_untracked_files_symbol='╋'}
+: ${has_adds_symbol='╋'}
+: ${has_deletions_symbol='✕'}
+: ${has_deletions_cached_symbol='✕'}
+: ${has_modifications_symbol='●'}
+: ${has_modifications_cached_symbol='●'}
+: ${ready_to_commit_symbol='★'}
+: ${detached_symbol='☢'}
+: ${no_upstream_symbol='⇞'}
+: ${is_on_a_tag_symbol='⌦'}
+: ${needs_to_merge_symbol='ᄉ'}
+: ${can_fast_forward_symbol='⇉'}
+: ${has_diverged_symbol='Ⴤ'}
+: ${rebase_tracking_branch_symbol='↸'}
+: ${merge_tracking_branch_symbol='ᄉ'}
+: ${should_push_symbol='↑'}
+
+# Colors
+: ${on='\033[1;37m'}
+: ${off='\033[0m'}
+: ${red='\033[0;31m'}
+: ${green='\033[0;32m'}
+: ${yellow='\033[1;33m'}
+: ${violet='\033[0;35m'}
+: ${branch_color='\033[1;34m'}
+: ${reset='\033[0m'}
+
