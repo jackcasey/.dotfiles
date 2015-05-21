b3dfb26ddfbbfa8faa25540283b0617d58f04d6d
Jack Casey <jack.casey@agworld.com.au>
2015-05-21 11:07:18 +0800

    Hammerspoon - Added HS and window mappings

---
+++ mac/hammerspoon.symlink/init.lua
@@ -0,0 +1,59 @@
+hs.window.animationDuration = 0
+
+-- Full screen
+hs.hotkey.bind({"cmd", "alt"}, "G", function()
+  local win = hs.window.focusedWindow()
+  win:setFrame(win:screen():frame())
+end)
+
+-- Cycle Left
+hs.hotkey.bind({"cmd", "alt"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  -- Full height
+  f.y = max.y
+  f.h = max.h
+
+  -- Cycle through 1/2, 1/3 and 2/3
+  if f.x == max.x and f.w == max.w / 2 then
+    f.x = max.x
+    f.w = max.w / 3
+  elseif f.x == max.x and f.w == max.w / 3 then
+    f.x = max.x
+    f.w = 2 * max.w / 3
+  else
+    f.x = max.x
+    f.w = max.w / 2
+  end
+
+  win:setFrame(f)
+end)
+
+-- Cycle Right
+hs.hotkey.bind({"cmd", "alt"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  -- Full height
+  f.y = max.y
+  f.h = max.h
+
+  -- Cycle through 1/2, 1/3 and 2/3
+  if f.x == max.w / 2 and f.w == max.w / 2 then
+    f.x = 2 * max.w / 3
+    f.w = max.w / 3
+  elseif f.x == 2 * max.w / 3 and f.w == max.w / 3 then
+    f.x = max.w / 3
+    f.w = 2 * max.w / 3
+  else
+    f.x = max.w / 2
+    f.w = max.w / 2
+  end
+
+  win:setFrame(f)
+end)
