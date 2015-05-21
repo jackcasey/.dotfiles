58d170d75acde24b2971615788b653e8c84a272c
Jack Casey <jack.casey@agworld.com.au>
2015-05-21 11:09:42 +0800

    Brew - moved brews into script from bootstrap text file

---
+++ brew_all.sh
@@ -0,0 +1,20 @@
+brew update && brew cleanup
+brew install git
+brew install bash-completion
+brew install psql
+brew install mongodb
+brew install caskroom/cask/brew-cask
+brew install ag
+brew install ctags
+brew install vim --with-lua
+brew install macvim --with-cscope --with-lua --HEAD
+brew install nvm
+
+brew upgrade brew-cask && brew cask cleanup
+brew cask install karabiner
+brew cask install iterm2
+brew cask install alfred
+brew cask install google-drive
+brew cask install dropbox
+brew cask install diffmerge
+brew cask install hammerspoon
