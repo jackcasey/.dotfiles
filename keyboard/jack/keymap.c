f966ff670a131c167ab7c0db5107d2a83a6bcd6a
Jack Casey <jack.casey@agworld.com.au>
2019-05-24 13:51:13 +0800

    Keyboard - update firmware mappings

--- keyboard/jack/keymap.c
+++ keyboard/jack/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QW] = LAYOUT_ortho_5x15( /* QWERTY */
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, LCTL(LCMD(KC_Q)),  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL, LCMD(KC_TAB),  KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL, LALT(KC_S),  KC_BSPC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LCMD(KC_LEFT),  KC_GRV,   LCMD(KC_RIGHT), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
     KC_LCTL, KC_LALT, KC_LGUI, MO(_FN1),KC_SPC,  KC_SPC,  MO(_FN2), LCMD(KC_SPC), KC_HYPR, KC_SPC, KC_SPC, MO(_FN1),KC_RGUI, KC_RALT, KC_RCTL  \
  ),
