dcbe526cba2abed7b129665d7e80f2766439d860
Jack Casey <jack.casey@agworld.com.au>
2019-12-09 12:08:59 +0800

    Remove some unused editor configs

--- atom/auto_setup.sh
+++
@@ -1,9 +0,0 @@
-source ./functions.sh
-
-mkdir -p $HOME/.atom
-
-safelink `pwd`/atom/config.cson $HOME/.atom/config.cson
-safelink `pwd`/atom/init.coffee $HOME/.atom/init.coffee
-safelink `pwd`/atom/keymap.cson $HOME/.atom/keymap.cson
-safelink `pwd`/atom/snippets.cson $HOME/.atom/snippets.cson
-safelink `pwd`/atom/styles.less $HOME/.atom/styles.less
